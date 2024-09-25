92770f5a7e6a26abf9bacce4833b779d9bac9f82
Chrstjan <ps5christian@hotmail.com>
2024-09-25 13:16:41 +0200

    Added background image & movie posters

--- Star Wars/src/App.jsx
+++ Star Wars/src/App.jsx
@@ -2,14 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useFetch } from "./hooks/useFetch";
 import { useImages } from "./hooks/useImages";
 import "./App.scss";
+import { MainLayout } from "./layouts/MainLayout";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route>
-            <Route />
+          <Route path={"/"} element={<MainLayout />}>
+            <Route index/>
           </Route>
         </Routes>
       </Router>
