f0e331f67715e032e1a9f484f2cc52091764f4e7
Chrstjan <ps5christian@hotmail.com>
2024-09-25 14:14:54 +0200

    Got data from api

--- Star Wars/src/pages/HomePage.jsx
+++ Star Wars/src/pages/HomePage.jsx
@@ -1,5 +1,39 @@
+import { useQuery } from "@tanstack/react-query"
+import { request } from "graphql-request"
+import { allFilms } from "../queries/allFilms"
+import { useEffect } from "react";
+
 export const HomePage = () => {
+
+  //Movie posters array
+  const moviePosters = ["./src/assets/images/star-wars-ep-4.png", "./src/assets/images/star-wars-ep-5.png", "./src/assets/images/star-wars-ep-6.png", "./src/assets/images/star-wars-ep-1.png", "./src/assets/images/star-wars-ep-2.png", "./src/assets/images/star-wars-ep-3.png"];
+
+  const {data, isLoading, error } = useQuery(
+    {
+      queryKey: ["All Films"],
+      queryFn: async () => request('https://swapi-graphql.netlify.app/.netlify/functions/index', allFilms)
+    }
+  );
+
+  useEffect(() => {
+    console.log("Film Data:", data);
+  }, [data])
+
+
+  //Temp testing code!!
   return (
-    <div>HomePage</div>
+    <div>
+      {data?.allFilms?.films?.map((item) => {
+      return (
+        <figure>
+          <h4 key={item.title}>{item.title}</h4>
+        </figure>
+      )
+    })}
+    {moviePosters.map((item) => {
+      return (
+        <img src={item} />
+      )
+    })}</div>
   )
 }
